43cbe251a38848823612ed5a3139371cf27cca6c
Sanjay-428 <sanjayofficial416@gmail.com>
2026-08-21 23:56:18 +0530

    force pushing with real code text

---
+++ testfile.js
@@ -0,0 +1,15 @@
+// Variables
+let name = "Sanjay";
+let age = 20;
+
+// Display in console
+console.log("Name: " + name);
+console.log("Age: " + age);
+
+// Function
+function greet() {
+    alert("Hello " + name);
+}
+
+// Call function
+greet();
